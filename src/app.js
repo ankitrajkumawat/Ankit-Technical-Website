4df1fc1d4f6efba74f247a92ef5642a54f420d86
ankitrajkumawat <ankitrkumawat24@gmail.com>
2022-01-12 17:31:35 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const path = require("path");
+require("./database/conn");
+const User = require("./models/usermessage");
+const app = express();
+const hbs = require("hbs");
+const { registerPartials } = require("hbs");
+const async = require("hbs/lib/async");
+const port = process.env.PORT || 3000;
+//setting the path
+const staticpath = path.join(__dirname, "../public(static)");
+const templatepath = path.join(__dirname, "../templates/views");
+const partialspath = path.join(__dirname, "../templates/partials");
+//middleware
+app.use('/css', express.static(path.join(__dirname, "../node_modules/bootstrap/dist/css")));
+app.use('/js', express.static(path.join(__dirname, "../node_modules/bootstrap/dist/js")));
+app.use('/jq', express.static(path.join(__dirname, "../node_modules/jquery/dist")));
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(staticpath))
+app.set("view engine", "hbs");
+app.set("views", templatepath);
+hbs.registerPartials(partialspath);
+//routing
+//app.get(path,callback)  just / means home page
+app.get("/", (req, res) => {
+    res.render("index");
+})
+app.get("/contact", (req, res) => {
+    res.render("contact");
+})
+app.post("/contact", async(req, res) => {
+        try {
+            // res.send(req.body);
+            const userData = new User(req.body);
+            await userData.save();
+            res.status(201).render("index");
+        } catch (error) {
+            res.status(500).send(error);
+            console.log(error);
+        }
+    })
+    //server creation
+app.listen(port, () => {
+    console.log("Server is up and running.");
+});
